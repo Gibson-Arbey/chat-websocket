e7438c8946f20ed860ef0eaa8a40f417c586f323
Gibson-Arbey <gibsonarbey@gmail.com>
2025-01-11 16:42:25 -0500

    se modifica la capa de negocio para enviar y recibir mensajes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,43 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ port: 3000 });
 
 wss.on('connection', function connection(ws) {
+
+  console.log('Cliente conectado');
+
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
-    console.log('received: %s', data);
+
+    const payload = JSON.stringify({
+      type: 'message',
+      payload: data.toString(),
+    })
+
+    //* Enviar mensaje a todos incluso al que envia el mensaje (emisor)
+    // wss.clients.forEach(function each(client) {
+    //   if (client.readyState === WebSocket.OPEN) {
+    //     client.send(payload, { binary: false });
+    //   }
+    // });
+
+    // * Enviar mensaje a todos excepto al que envia el mensaje (emisor)
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(payload, { binary: false });
+      }
+    });
+    
+
   });
 
-  ws.send('something');
+  // * evento de que un usuario de desconecto   
+  ws.on('close', () => {
+    console.log('Cliente desconectado');
+  })
+
 });
+
+
+console.log('ws://localhost:3000');
